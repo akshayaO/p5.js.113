66f1dbc5917b20c8be4099fa6d48e681f5748a1b
akshayaO <79805752+akshayaO@users.noreply.github.com>
2022-05-26 17:19:22 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,27 +4,32 @@ function preload(){
 function setup(){
     canvas = createCanvas(640,480);
     canvas.position(110,250);
+    video = createCapture(VIDEO);
+    video.hide();
 }
 
 
-function draw(){
-    
-// Draw a circle at location (30, 30) with a diameter of 20.
-circle(80, 90, 80);
-describe('red circle with black outline ');
-}
+
+
+
+
 
 function draw(){
     
     // Draw a circle at location (30, 30) with a diameter of 20.
+   fill (255, 90, 90); stroke(255, 90, 90);
     circle(50, 50, 90);
     circle(50, 430, 90);
     circle(590, 50, 90);
     circle(590, 430, 90);
+    rect(85, 25, 539, 55);
+    rect(25, 80, 55, 330);
+    rect(565, 80, 55, 330);
+    rect(85, 400, 539, 55);
     }
 
 
 function take_snapshot(){
-    save('student.png');
+    save('pinkFrame.png');
 }
 ;
